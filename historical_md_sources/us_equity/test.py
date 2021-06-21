c9ee361cda7050192eafd3d34a42f7330dfc7b6b
leisuretrader <66764406+leisuretrader@users.noreply.github.com>
2021-06-21 06:46:52 -0400

    Delete historical_md_sources/us_equity directory

--- historical_md_sources/us_equity/test.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-from datetime import datetime
-my_date = date.today()
-
-print (my_date)
-my_time = datetime.min.time()
-my_datetime = datetime.combine(my_date, my_time)
-print(my_datetime.isoformat())
